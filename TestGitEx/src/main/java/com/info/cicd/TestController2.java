80b6ff71754a172c385c33a616612fe27ba52242
lakshmikundeti99 <lakshmikundeti99@gmail.com>
2025-07-02 17:06:21 -0400

    added new file

---
+++ TestGitEx/src/main/java/com/info/cicd/TestController2.java
@@ -0,0 +1,20 @@
+package com.info.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController2 {
+	
+	@GetMapping("/test3")
+	public String getMsg() {
+		return "Welcome cicd TestController2";
+	}
+	
+	
+	@GetMapping("/test2")
+	public String getMsg1() {
+		return "Welcome cicd TestController2 test2 end point";
+	}
+
+}
