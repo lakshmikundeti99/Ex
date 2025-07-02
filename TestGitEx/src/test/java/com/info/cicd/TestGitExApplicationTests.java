3607f3de7faceaa794569e0a2e8187487b3f17cd
lakshmikundeti99 <lakshmikundeti99@gmail.com>
2025-07-02 16:40:46 -0400

    ex commit

---
+++ TestGitEx/src/test/java/com/info/cicd/TestGitExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
